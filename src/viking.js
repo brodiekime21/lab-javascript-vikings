e94d695d763f8f5c8e8aa1ea59fe6af2f931993e
Brodie Kime <brodie.kime@gmail.com>
2023-01-13 17:29:23 -0500

    done Brodie Kime

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,83 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+      this.health = health;
+      this.strength = strength;
+    }  
+    
+    attack(){
 
-// Viking
-class Viking {}
+        return this.strength;
+    }      
 
-// Saxon
-class Saxon {}
+    receiveDamage(damage){
+        this.health -= damage;
+        }
+    }
 
-// War
-class War {}
+
+  // Viking
+//how does the recieve damage work without us calling it again
+class Viking extends Soldier {
+    constructor(name, health , strength){
+        super(name, health, strength);
+        this.name = name;
+        this.health=health;
+        this.strength = strength;
+    }
+    receiveDamage(damage){
+        this.health -= damage;
+        
+        if (this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else {
+        return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry(){
+
+        return "Odin Owns You All!";
+    }    
+
+}
+
+
+
+
+
+  
+  // Saxon
+  class Saxon extends Soldier {
+    receiveDamage(damage){
+        this.health -= damage;
+        
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else {
+        return `A Saxon has died in combat`
+        }
+    }
+
+
+  }
+  
+
+
+  // War
+  class War {
+    constructor(vikingArmy, saxonArmy){
+    this.vikingArmy =[];
+    this.saxonArmy =[];
+  }
+  addViking(oneMoreViking){
+    
+    this.vikingArmy += oneMoreViking;
+
+  }
+
+  addSaxon(oneMoreSaxon){
+
+  }
+}
